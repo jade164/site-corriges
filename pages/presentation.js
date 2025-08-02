db588fdee14b1acbb6953c73fc378fb1b427076d
jade164 <jade.guittard78@gmail.com>
2025-08-02 17:28:20 +0200

    Add files via upload

---
+++ pages/presentation.js
@@ -0,0 +1,14 @@
+
+import Layout from '../components/Layout';
+
+export default function Presentation() {
+  return (
+    <Layout>
+      <h2 className="text-2xl font-bold mb-4">Présentation</h2>
+      <p className="text-gray-700">
+        Bienvenue sur SuperBac Maths ! Ce site est conçu pour accompagner les élèves dans leur préparation au baccalauréat en mathématiques. 
+        Vous y trouverez des cours, des annales corrigées, ainsi que des ressources pour progresser efficacement.
+      </p>
+    </Layout>
+  );
+}
